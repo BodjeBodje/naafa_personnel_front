91e86227aaa79db99458ad465b88a24ae8b88d21
MacBook Pro <macbookpro@MacBookPro.nafa-employee.sn>
2025-12-16 16:53:33 +0000

    .end

--- src/services/api/AppApi.ts
+++ src/services/api/AppApi.ts
@@ -1,8 +1,9 @@
 import axios, { AxiosError } from 'axios';
 
-const API_BASE_URL = 'http://192.168.2.252:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 // const API_BASE_URL = 'http://127.0.0.1:8000/api';
-export const STORAGE_BASE_URL = 'http://192.168.2.76:8000/storage';
+// export const STORAGE_BASE_URL = 'http://192.168.2.76:8000/storage';
+export const STORAGE_BASE_URL = import.meta.env.VITE_STORAGE_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
